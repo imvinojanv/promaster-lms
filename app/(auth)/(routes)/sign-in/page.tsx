f17b290809c5c1a8f29dbd817ac562f83e637f5d
Vinojan1999 <vinojan02abhimanyu@gmail.com>
2023-11-29 12:27:04 +0530

    Authentication 🔐

--- app/(auth)/(routes)/sign-in/page.tsx
+++
@@ -1,9 +0,0 @@
-const SigninPage = () => {
-    return (  
-        <div>
-            This is unprotected page
-        </div>
-    );
-}
- 
-export default SigninPage;
--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,9 +1,9 @@
-
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
-      This is protected page
+      <UserButton afterSignOutUrl="/"/>
     </div>
   )
 }
