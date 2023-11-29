46b6365e01e2e5ce1d39de1f5b2579957752aae0
Vinojan1999 <vinojan02abhimanyu@gmail.com>
2023-11-29 20:40:17 +0530

    Mobile Sidebar & Navbar 🪟📱

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from "./_components/Navbar";
 import Sidebar from "./_components/Sidebar";
 
 const DashboardLayout = ({
@@ -7,6 +8,9 @@ const DashboardLayout = ({
 }) => {
     return (
         <div className="h-full">
+            <div className="h-[80px] md:pl-56 fixed inset-y-0 w-full z-50">
+                <Navbar />
+            </div>
             <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
                 <Sidebar />
             </div>
