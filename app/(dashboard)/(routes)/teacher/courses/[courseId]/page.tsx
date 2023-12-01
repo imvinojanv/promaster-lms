81d553f23570376d756959d66350d85496c05ace
Vinojan1999 <vinojan02abhimanyu@gmail.com>
2023-12-01 19:10:51 +0530

    Course edit page UI 🪟

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,13 +1,69 @@
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { LayoutDashboard } from "lucide-react";
 
-// Every server component inside this folder can access the params
-const CourseIdPage = ({
-    params
-} : {
-    params: { courseId: string }
+import { db } from "@/lib/db";
+import { IconBadge } from "@/components/icon-badge";
+
+const CourseIdPage = async ({
+  // Every server component inside this folder can access the params
+  params
+}: {
+  params: { courseId: string }
 }) => {
+  const { userId } = auth();
+
+  // If there is no any userId
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const course = await db.course.findUnique({
+    where: {
+      id: params.courseId
+    }
+  });
+
+  // If don't find the course
+  if (!course) {
+    return redirect("/");
+  }
+
+  const requiredFields = [
+    course.title,
+    course.description,
+    course.imageUrl,
+    course.price,
+    course.categoryId,
+  ];
+
+  const totalFields = requiredFields.length;
+  const completedFields = requiredFields.filter(Boolean).length;
+
+  const completionText = `(${completedFields}/${totalFields})`;     // => (2/6)
+
   return (
-    <div>
-        Course Id Page: {params.courseId}
+    <div className="p-6">
+      <div className="flex items-center justify-between">
+        <div className="flex flex-col gap-y-2">
+          <h1 className="text-2xl font-medium">
+            Course setup
+          </h1>
+          <span className="text-sm text-slate-700">
+            Complete all fields {completionText}
+          </span>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+        <div>
+          <div className="flex items-center gap-x-2">
+            <IconBadge icon={LayoutDashboard} />
+            <h2 className="text-xl">
+              Customize your course
+            </h2> 
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
