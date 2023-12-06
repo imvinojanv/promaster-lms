8d2ea51149ca761e3e5ea00a77686452b93274a8
Vinojan1999 <vinojan02abhimanyu@gmail.com>
2023-12-06 16:57:28 +0530

    Chapter Edit Page Title✍️

---
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -0,0 +1,49 @@
+
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+import { db } from "@/lib/db";
+
+export async function PATCH(
+    req: Request,
+    { params }: { params: { courseId: string; chapterId: string } }
+) {
+    try {
+        const { userId } = auth();
+        // Chapter publishing will be controlled by a separate API route, which is going to check whethere we have all the necessary fields
+        const { isPublished, ...values } = await req.json();
+
+        if (!userId) {
+            return new NextResponse("Unauthorized", { status: 401 });
+        }
+
+        const ownCourse = await db.course.findUnique({
+            where: {
+                id: params.courseId,
+                userId
+            }
+        });
+
+        if (!ownCourse) {
+            return new NextResponse("Unauthorized", { status: 401 });
+        }
+
+        const chapter = await db.chapter.update({
+            where: {
+                id: params.chapterId,
+                courseId: params.courseId,
+            },
+            data: {
+                ...values,
+            }
+        });
+
+        // TODO: Handle Video Upload
+
+        return NextResponse.json(chapter);
+
+    } catch (error) {
+        console.log("[COURSES_CHAPTER_ID]", error);
+        return new NextResponse("Internal Error", { status: 500 });
+    }
+}
