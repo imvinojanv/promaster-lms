3492b72ddc7b4c6f2edebfec049b0ab0b947f4c3
Vinojan1999 <vinojan02abhimanyu@gmail.com>
2023-12-23 11:59:11 +0530

    Chapter HLS Video Player 🎞️

--- app/(course)/courses/[courseId]/page.tsx
+++ app/(course)/courses/[courseId]/page.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
 
-const CourseIdPage = () => {
-  return (
-    <div>CoursePage</div>
-  )
+const CourseIdPage = async ({
+  params
+}: {
+  params: { courseId: string }
+}) => {
+  const course = await db.course.findUnique({
+    where: {
+      id: params.courseId,
+    },
+    include: {
+      chapters: {
+        where: {
+          isPublished: true,
+        },
+        orderBy: {
+          position: "asc"
+        }
+      }
+    }
+  });
+
+  if (!course) {
+    return redirect("/");
+  }
+
+  // Immediately redirect to the first chapter
+  return redirect(`/courses/${course.id}/chapters/${course.chapters[0].id}`);
 }
 
 export default CourseIdPage
