3fdf9b1bf22f27b589d5e7c8b2ca7ded6f14e3ed
Vinojan1999 <vinojan02abhimanyu@gmail.com>
2024-01-24 12:29:32 +0530

    Teacher Analytics Dashboard 🧑‍🏫📈

---
+++ app/(dashboard)/(routes)/teacher/analytics/_components/data-card.tsx
@@ -0,0 +1,36 @@
+import {
+    Card,
+    CardContent,
+    CardHeader,
+    CardTitle
+} from "@/components/ui/card";
+import { priceFormat } from "@/lib/format";
+
+interface DataCardProps {
+    value: number;
+    label: string;
+    shouldFormat?: boolean;
+}
+
+const DataCard = ({
+    value,
+    label,
+    shouldFormat,
+}: DataCardProps) => {
+    return (
+        <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                <CardTitle className="text-sm font-medium">
+                    {label}
+                </CardTitle>
+            </CardHeader>
+            <CardContent>
+                <div className="text-2xl font-bold">
+                    {shouldFormat ? priceFormat(value) : value}
+                </div>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default DataCard
